94757dfa625f64e09579801278062e533de46f0d
user <khoury.ichak@gmail.com>
2017-10-25 16:16:34 +0300

    Create a project as first draft that read data from database and return back a json array that represent many records

---
+++ src/main/java/com/solugence/mediators/db/select/DBSelectMediator.java
@@ -0,0 +1,68 @@
+package com.solugence.mediators.db.select;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.mediators.db.AbstractDBMediator;
+import org.apache.synapse.mediators.db.Statement;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class DBSelectMediator extends AbstractDBMediator
+{
+
+	protected void processStatement(Statement stmnt, MessageContext msgCtx)
+	{
+		Connection con = null;
+		ResultSet rs = null;
+		try
+		{
+			con = this.getDataSource().getConnection();
+			PreparedStatement ps = getPreparedStatement(stmnt, con, msgCtx);
+			rs = ps.executeQuery();
+
+			JSONArray resultASJSONARR 	= new JSONArray();
+			JSONObject jsonObject		= null;
+			while (rs.next())
+			{
+				jsonObject = new JSONObject();
+				for (String propName : stmnt.getResultsMap().keySet())
+				{
+					jsonObject.put(propName, rs.getString(propName));
+				}
+				resultASJSONARR.put(jsonObject);
+			}
+			msgCtx.setProperty(DBSelectMediatorConstants.DB_SELECT_PROPERTY_NAME, resultASJSONARR.toString());
+		} 
+		catch (SQLException e)
+		{
+			log.error("Error executing SQL statement : " + stmnt.getRawStatement() + " against DataSource : "
+					+ getDSName(), e);
+		} finally
+		{
+			if (rs != null)
+			{
+				try
+				{
+					rs.close();
+				} catch (SQLException e)
+				{
+					log.warn("Error occurred while trying to close result set.", e);
+				}
+			}
+			if (con != null)
+			{
+				try
+				{
+					con.close();
+				} catch (SQLException e)
+				{
+					log.warn("Error occurred while trying to close connection.", e);
+				}
+			}
+		}
+	}
+}
